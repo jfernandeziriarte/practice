754fa630cff0d0fff2819fb912fe0e3523cc1480
Jorge <jorge@MacBook-de-Jorge.local>
2014-10-16 13:36:43 +0200

    Fixed sitaxis in elifs. Last print out of if.

--- respuesta_personalizada.py
+++ respuesta_personalizada.py
@@ -6,10 +6,9 @@ name = raw_input("Cual es tu nombre? ")
 if len(name) == 0 or name.isdigit():
 	print "En realidad SI tienes que escribir tu nombre (el de pila), prueba otra vez"
 	name = raw_input("Cual es tu nombre? ")
-#elseifs commented. Want to give personal messages with 'in'.
-#elseif name in [luis, Luis, LUIS]:
-#	print "Hola Luis. Tu nombre me hace indicar que eres un mambru. No lo niegues."
-#elseif name in [jorge, Jorge, JORGE]:
-#	print "Tu cara me suena. Bienvenido, maestro."
-else:
-	print "Encantado de conocerte, %s. Conozcamonos mejor." % name
+elif name in ("luis", "Luis", "LUIS"):
+	print "Hola Luis. Tu nombre me hace indicar que eres un mambru. No lo niegues."
+elif name in ("jorge", "Jorge", "JORGE"):
+	print "Tu cara me suena. Bienvenido, maestro."
+
+print "Encantado de conocerte, %s. Conozcamonos mejor." % name
